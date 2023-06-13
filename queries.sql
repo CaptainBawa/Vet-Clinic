658f24cbb151a4551c385270885012c7e9d5f017
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-13 23:41:30 +0000

    Update species column

--- queries.sql
+++ queries.sql
@@ -24,3 +24,12 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg 
 -- (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- New
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have 
+-- species already set.
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
