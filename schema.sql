d1b2e44aa23220486cf4808186baf67b30002461
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-13 23:05:08 +0000

    Add new data

--- schema.sql
+++ schema.sql
@@ -6,5 +6,8 @@ CREATE TABLE animals (
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN DEFAULT FALSE,
-    weight_kg NUMERIC
+    weight_kg NUMERIC,
 );
+-- Adding new table header
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(100);
