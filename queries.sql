73420493e4856f481dc42f2954dafbdbb1821025
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-14 04:10:15 +0000

    Add basic maths query

--- queries.sql
+++ queries.sql
@@ -67,3 +67,27 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 -- Commit transaction
 COMMIT;
 
+-- Write queries to answer the following questions:
+-- How many animals are there?
+-- How many animals have never tried to escape?
+-- What is the average weight of animals?
+-- Who escapes the most, neutered or not neutered animals?
+-- What is the minimum and maximum weight of each type of animal?
+-- What is the average number of escape attempts per animal type of those 
+-- born between 1990 and 2000?
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, COUNT(*) FROM animals WHERE escape_attempts > 0 GROUP BY neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT MIN(weight_kg) FROM animals;
+SELECT MAX(weight_kg) FROM animals;
+-- What is the average number of escape attempts per animal type of those 
+-- born between 1990 and 2000?
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31';
+
