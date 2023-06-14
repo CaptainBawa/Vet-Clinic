1c40b7df50151c99a0510415fd0577068632f896
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-14 20:26:35 +0000

    Create 2 new tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,25 @@ CREATE TABLE animals (
 -- Adding new table header
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(100);
+
+---------------------------------------------------------------------------------------------
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer
+
+CREATE TABLE owners (
+    owner_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INT
+);
+
+---------------------------------------------------------------------------------------------
+-- Create a table named species with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
