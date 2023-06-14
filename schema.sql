ab0ee8461ddd3e44d973e0aafb362d1361206469
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-14 21:42:59 +0000

    Add foreign key and new columns to the animals table

--- schema.sql
+++ schema.sql
@@ -33,3 +33,33 @@ CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     name VARCHAR(100)
 );
+
+---------------------------------------------------------------------------------------------
+
+-- Modify animals table:
+-- Make sure that id is set as autoincremented PRIMARY KEY
+-- Remove column species
+-- Add column species_id which is a foreign key referencing species table
+-- Add column owner_id which is a foreign key referencing the owners table
+
+-- Remove column species
+ALTER TABLE animals DROP COLUMN IF EXISTS species;
+
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_animals_species
+FOREIGN KEY (species_id)
+REFERENCES species(species_id);
+
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_animals_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(owner_id);
+
+
+
