fd7ab9bca3885ba90418dd9ff101fdbec28adb4f
Collins Bawa <collinskwamebawa@yahoo.com>
2023-06-15 21:11:28 +0000

    Create specializations and visits table

--- schema.sql
+++ schema.sql
@@ -68,12 +68,36 @@ ALTER TABLE species RENAME COLUMN species_id TO id;
 
 
 ---------------------------------------------------------------------------------------------
-
+-- Create a table named vets with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+-- age: integer
+-- date_of_graduation: date
 CREATE TABLE vets (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     age INT,
     date_of_graduation DATE
+);
+
+-- There is a many-to-many relationship between the tables species and vets: 
+-- a vet can specialize in multiple species, and a species can have multiple 
+-- vets specialized in it. Create a "join table" called specializations to 
+-- handle this relationship.
+
+CREATE TABLE specializations (
+    species_id INT,
+    vets_id INT
+);
+
+-- There is a many-to-many relationship between the tables animals and vets: 
+-- an animal can visit multiple vets and one vet can be visited by multiple 
+-- animals. Create a "join table" called visits to handle this relationship, 
+-- it should also keep track of the date of the visit.
+
+CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT
 );
 
 
